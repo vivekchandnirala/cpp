455a1aace7ec25a2329089ef9435e61918fb5a04
VIVEK CHAND NIRALA <121417084+vivekchandnirala@users.noreply.github.com>
2025-08-15 16:36:09 +0530

    Create maxOf2Num.cpp

---
+++ maxOf2Num.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "Enter the Numbers: ";
+    cin>> a >>b;
+    cout << "Maximum number is: " ;
+    if(a>b){
+        cout<<a;
+    }else{
+        cout<<b;
+    }
+    return 0;
+}
