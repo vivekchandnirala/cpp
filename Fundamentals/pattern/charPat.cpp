8efdedfe9865618080b49bb2b1b7fc58476e6571
Vivek Chand Nirala <vivekchandnirala1@gmail.com>
2025-09-09 23:08:38 +0530

    Patterns Questions

---
+++ Fundamentals/pattern/charPat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the Number: ";
+    cin >> n;
+    
+    for (int i=0; i<n; i++){
+        char ch = 'A';
+        for(int j=0; j<n; j++){
+            cout << ch<< " ";
+            ch++;
+        }
+        cout <<endl;
+    }
+    return 0;
+}
