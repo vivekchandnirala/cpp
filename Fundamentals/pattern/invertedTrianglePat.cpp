2d8b7cd38a92c92b42f9c9476d57bfaf54d60255
Vivek Chand Nirala <vivekchandnirala1@gmail.com>
2025-09-10 14:44:14 +0530

    Patterns Questions Done

---
+++ Fundamentals/pattern/invertedTrianglePat.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the Number: ";
+    cin >> n;
+    char ch = 'A';
+    for(int i=0; i<=n; i++){
+        for (int j=0; j<=i; j++){
+            cout <<" ";
+        }
+        for(int j=n;j>i;j--){
+            cout<< ch;
+        }
+        ch++;
+            cout << endl;
+
+    }
+    
+    return 0;
+}
---
+++ Fundamentals/pattern/numTrianglePat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the Number: ";
+    cin >> n;
+    
+    for (int i=0; i<n; i++){
+        for(int j=0; j<(i+1); j++){
+            cout << (i+1)<< " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
